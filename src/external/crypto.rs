a4c51c5d6246ff8d36ee1a5a97890c4cb6aa14c8
Jeff Simpson <jsimpson@kochava.com>
2018-12-04 14:00:49 -0500

    refactor(crypto): renamed external crypto function

--- src/external/crypto.rs
+++ src/external/crypto.rs
@@ -1,7 +1,7 @@
 use super::externs::{_get_key};
 
 /// Calls a host function to Sha3_256 data and return the hash
-pub fn host_sha256(data: Vec<u8>) -> Vec<u8> {
+pub fn sha256(data: Vec<u8>) -> Vec<u8> {
     // Create Vec to store hash
     let mut hash = Vec::with_capacity(32 as usize); // 32 byte (256) hash
     unsafe { hash.set_len(32 as usize) };

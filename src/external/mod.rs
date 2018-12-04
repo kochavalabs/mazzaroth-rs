19e602712e9a814cd455077157b67920444ff0fd
Jeff Simpson <jsimpson@kochava.com>
2018-12-04 15:05:09 -0500

    refactor(external): moved transaction related functions to own module

--- src/external/mod.rs
+++ src/external/mod.rs
@@ -4,19 +4,4 @@ pub mod persistence;
 
 pub mod crypto;
 
-/// Get the arguments encoded from the Runtime
-pub fn arguments() -> Vec<u8> {
-    let length = unsafe { externs::_input_length() };
-    let mut args: Vec<u8> = Vec::with_capacity(length as usize);
-    unsafe {
-        args.set_len(length as usize);
-        externs::_fetch_input(args.as_mut_ptr());
-    }
-
-    args
-}
-
-/// Return encoded bytes to the runtime
-pub fn ret(values: Vec<u8>) {
-    unsafe { externs::_ret(values.as_ptr(), values.len()) };
-}
+pub mod transaction;
---
+++ src/external/transaction.rs
@@ -0,0 +1,18 @@
+use super::externs::{_input_length, _fetch_input, _ret};
+
+/// Get the arguments encoded from the Runtime
+pub fn arguments() -> Vec<u8> {
+    let length = unsafe { _input_length() };
+    let mut args: Vec<u8> = Vec::with_capacity(length as usize);
+    unsafe {
+        args.set_len(length as usize);
+        _fetch_input(args.as_mut_ptr());
+    }
+
+    args
+}
+
+/// Return encoded bytes to the runtime
+pub fn ret(values: Vec<u8>) {
+    unsafe { _ret(values.as_ptr(), values.len()) };
+}

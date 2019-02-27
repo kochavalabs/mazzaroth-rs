82c063b508962151e65051838d330c92ba59f68c
Kyle Bartush <kbartush@kochava.com>
2019-02-26 17:32:55 -0800

    feat(signing): add wrappers around host function calls for signing
    Initial wrappers around host functions invloved with signing. These
    still need to be tested within the rothvm and I expect that further
    testing will uncover bugs. Allows users to:
    
    - Generate a elliptic curve key pair
    - Sign a message with a private key
    - Validate a signature with a public key
    
    Breaks nothing

--- src/external/crypto.rs
+++ src/external/crypto.rs
@@ -1,4 +1,7 @@
-use super::externs::{_sha256, _sha3_256, _sha3_512, _keccak256, _shake256};
+use super::externs::{
+    _generate_key_pair, _keccak256, _sha256, _sha3_256, _sha3_512, _shake256, _sign_message,
+    _validate_signature,
+};
 
 /// Calls a host function to Sha256 data and return the hash
 pub fn sha256(data: Vec<u8>) -> Vec<u8> {
@@ -55,3 +58,60 @@ pub fn shake256(data: Vec<u8>) -> Vec<u8> {
     hash
 }
 
+/// Host hashing function for generating a cryptographic key pair.
+/// Currently returns a P256 elliptic curve key pair, 32byte private key
+/// and 64 byte public key
+pub fn generate_key_pair() -> Result<(Vec<u8>, Vec<u8>), &'static str> {
+    let mut priv_key = Vec::with_capacity(32 as usize);
+    let mut pub_key = Vec::with_capacity(64 as usize);
+    unsafe { priv_key.set_len(32 as usize) };
+    unsafe { pub_key.set_len(64 as usize) };
+
+    unsafe { _generate_key_pair(priv_key.as_mut_ptr(), pub_key.as_mut_ptr()) };
+
+    match priv_key.iter().any(|x| *x != 0x0u8) {
+        true => Ok((priv_key, pub_key)),
+        false => Err("Problem generating key pair."),
+    }
+}
+
+/// Signs a message using the provided private key. You typically wouldn't be
+/// signing something by sending your private key to the network, so this is
+/// mostly for demonstration purposes.
+/// It uses a 32 byte P256 elliptic curve private key and returns a 64 byte
+/// signature.
+pub fn sign_message(priv_key: Vec<u8>, message: Vec<u8>) -> Result<Vec<u8>, &'static str> {
+    let mut signature = Vec::with_capacity(64 as usize);
+    unsafe { signature.set_len(64 as usize) };
+
+    unsafe {
+        _sign_message(
+            priv_key.as_ptr(),
+            message.as_ptr(),
+            message.len(),
+            signature.as_mut_ptr(),
+        )
+    };
+
+    match signature.iter().any(|x| *x != 0x0u8) {
+        true => Ok(signature),
+        false => Err("Problem signing message."),
+    }
+}
+
+/// Validates a signature using the provided public key. A Mazzaroth user's
+/// account address can be used as the public key to verify transactions sent
+/// from that user.
+/// We are currently using a 64 byte P256 elliptic curve public key and a 64
+/// byte signature string.
+pub fn validate_signature(pub_key: Vec<u8>, message: Vec<u8>, signature: Vec<u8>) -> bool {
+    let result = unsafe {
+        _validate_signature(
+            pub_key.as_ptr(),
+            message.as_ptr(),
+            message.len(),
+            signature.as_ptr(),
+        )
+    };
+    result != 0
+}

2274afd496a3a89de643f4a717f3a30fac41ecba
Jeff Simpson <jsimpson@kochava.com>
2019-05-16 16:53:13 -0400

    refactor(account): removed redundant account from function names

--- src/external/account.rs
+++ src/external/account.rs
@@ -1,7 +1,7 @@
 use super::externs::{_get_account_name, _get_account_name_length, _set_account_name, _get_account_balance, _set_account_balance};
 
 /// Get the value associated with a string key from the persistent storage for this runtime.
-pub fn get_account_name(key: Vec<u8>) -> Vec<u8> {
+pub fn get_name(key: Vec<u8>) -> Vec<u8> {
     let len = unsafe { _get_account_name_length(key.as_ptr(), key.len()) };
     let mut val = Vec::with_capacity(len as usize);
     unsafe { val.set_len(len as usize) };
@@ -10,17 +10,17 @@ pub fn get_account_name(key: Vec<u8>) -> Vec<u8> {
 }
 
 /// Store an account name in the persistent storage for this runtime.
-pub fn set_account_name(key: Vec<u8>, val: Vec<u8>) {
+pub fn set_name(key: Vec<u8>, val: Vec<u8>) {
     unsafe { _set_account_name(key.as_ptr(), key.len(), val.as_ptr(), val.len()) };
 }
 
 /// Get the balance associate with an account in the persistent storage for this runtime.
-pub fn get_account_balance(key: Vec<u8>) -> u64 {
+pub fn get_balance(key: Vec<u8>) -> u64 {
     let bal = unsafe { _get_account_balance(key.as_ptr(), key.len()) };
     bal
 }
 
 /// Set the balance associate with an account in the persistent storage for this runtime.
-pub fn set_account_balance(key: Vec<u8>, balance: u64) {
+pub fn set_balance(key: Vec<u8>, balance: u64) {
     unsafe { _set_account_balance(key.as_ptr(), key.len(), balance) };
 }

8f18279fb630ad9c55aeeae751219327f9a8a1bb
Jeff Simpson <jsimpson@kochava.com>
2019-05-17 14:58:05 -0400

    refactor(account): updated account functions to take and return String for name

--- src/external/account.rs
+++ src/external/account.rs
@@ -1,16 +1,20 @@
 use super::externs::{_get_account_name, _get_account_name_length, _set_account_name, _get_account_balance, _set_account_balance};
+use std::str;
 
 /// Get the value associated with a string key from the persistent storage for this runtime.
-pub fn get_name(key: Vec<u8>) -> Vec<u8> {
+pub fn get_name(key: Vec<u8>) -> String {
     let len = unsafe { _get_account_name_length(key.as_ptr(), key.len()) };
     let mut val = Vec::with_capacity(len as usize);
     unsafe { val.set_len(len as usize) };
     unsafe { _get_account_name(key.as_ptr(), key.len(), val.as_mut_ptr()) };
-    val
+    // Convert name to String
+    let result = str::from_utf8(&val).unwrap().to_owned();
+    result
 }
 
 /// Store an account name in the persistent storage for this runtime.
-pub fn set_name(key: Vec<u8>, val: Vec<u8>) {
+pub fn set_name(key: Vec<u8>, name: String) {
+    let val = name.into_bytes();
     unsafe { _set_account_name(key.as_ptr(), key.len(), val.as_ptr(), val.len()) };
 }
 

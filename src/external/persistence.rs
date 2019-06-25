c05747061f87120e9c2d0e47f2f249a67684ba0d
Kyle Bartush <kbartush@kochava.com>
2019-06-25 09:31:09 -0700

    reafactor(naming): switch get_exists to key_exists
    get_exists feels like an awkward naming so we're moving to key_exists
    
    Breaks nothing

--- src/external/persistence.rs
+++ src/external/persistence.rs
@@ -1,9 +1,9 @@
-use super::externs::{_get, _get_exists, _get_length, _store};
+use super::externs::{_get, _get_length, _key_exists, _store};
 use super::ExternalError;
 
 /// Get the value associated with a string key from the persistent storage for this runtime.
 pub fn get(key: Vec<u8>) -> Result<Vec<u8>, ExternalError> {
-    let exists = unsafe { _get_exists(key.as_ptr(), key.len()) };
+    let exists = unsafe { _key_exists(key.as_ptr(), key.len()) };
     if exists {
         let len = unsafe { _get_length(key.as_ptr(), key.len()) };
         let mut val = Vec::with_capacity(len as usize);

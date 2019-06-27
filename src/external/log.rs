211f35d76606cf83c541beca645a47ccad6fe598
Jeff Simpson <jsimpson@kochava.com>
2019-06-27 09:07:14 -0400

    refactor(externs): fixed warnings in library by switching to pass string as pointer and length

--- src/external/log.rs
+++ src/external/log.rs
@@ -2,5 +2,6 @@ use super::externs::_log;
 
 // public log function that wraps the host call
 pub fn log(msg: String) {
-    unsafe { _log(msg) }
+    let val = msg.into_bytes();
+    unsafe { _log(val.as_ptr(), val.len()) }
 }

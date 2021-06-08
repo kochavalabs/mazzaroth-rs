a06bf6dec65828fbe221f226e895c8fd5de074a5
lightyear15 <lightyear15@users.noreply.github.com>
2021-06-08 10:02:43 +0200

    adding TransactionInfo trait and default implementation (#43)
    adding TransactionInfo trait and default implementation

--- src/external/transaction.rs
+++ src/external/transaction.rs
@@ -94,6 +94,31 @@ pub fn sender() -> Vec<u8> {
     }
 }
 
+pub trait TransactionInfo {
+    fn arguments(&self) -> Vec<u8>;
+    fn ret(&self, _values: &[u8]);
+    fn sender(&self) -> Vec<u8>;
+}
+
+#[derive(Clone)]
+pub struct TransactionInfoImpl {}
+impl TransactionInfo for TransactionInfoImpl {
+    fn arguments(&self) -> Vec<u8> {
+        arguments()
+    }
+    fn ret(&self, values: &[u8]) {
+        ret(values.to_vec())
+    }
+    fn sender(&self) -> Vec<u8> {
+        sender()
+    }
+}
+impl Default for TransactionInfoImpl {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
 #[cfg(test)]
 #[cfg(feature = "host-mock")]
 mod tests {
